8034d0f77ea6aee9a2cc16fdc9e65b1ac430a48c
clement.darme <clement.darme@epita.fr>
2026-09-15 21:14:34 +0200

    FIX : fixed invalid behavior in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,18 +19,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
+    <html 
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
-}
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
       <body>
         <Script async 
           src="https://www.googletagmanager.com/gtag/js?id=G-D28WE3XDS0"
